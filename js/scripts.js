6f904f3de29546ebe24eb87d181646878993adb7
akokarpov <akokarpov@gmail.com>
2021-03-03 14:16:16 +0300

    2 pages completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 let button = document.getElementById('menu_button');
 let navigation = document.getElementById('navigation');
-let brand_overlay = document.getElementById('brand_overlay');
+let close_nav_button = document.getElementById('close_nav_button');
 
-button.addEventListener('click', function(){ 
-  brand_overlay.classList.toggle("brand_overlay")
+button.addEventListener('click', function () {
+  navigation.classList.toggle('header__navigation_display_block');
+});
+
+close_nav_button.addEventListener('click', function () {
   navigation.classList.toggle('header__navigation_display_block');
 });
