c70368b61b227f36b804dc0718af897c59e4ff2e
akokarpov <akokarpov@gmail.com>
2021-03-19 17:53:36 +0300

    completed catalog.html using css grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 let button = document.getElementById('menu_button');
 let close_nav_button = document.getElementById('close_nav_button');
+let filter_button = document.getElementById('filter_button');
+let filter_button_close = document.getElementById('filter_button_close');
 
 button.addEventListener('click', function () {
   navigation.classList.toggle('header__nav_display-block');
@@ -9,4 +11,13 @@ button.addEventListener('click', function () {
 close_nav_button.addEventListener('click', function () {
   navigation.classList.toggle('header__nav_display-block');
   nav_overlay.classList.toggle("header__nav-overlay_block")
+});
+
+
+filter_button.addEventListener('click', function () {
+filter_menu.classList.toggle('catalog__display_block');
+});
+
+filter_button_close.addEventListener('click', function () {
+filter_menu.classList.toggle('catalog__display_block');
 });
